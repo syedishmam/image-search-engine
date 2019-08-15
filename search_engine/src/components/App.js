d92ac3e315a4a07d6ee767d96a5e6b3f8e497343
Ish <syedishmam17@gmail.com>
2019-08-15 19:05:52 -0400

    Fix CORS error

--- search_engine/src/components/App.js
+++ search_engine/src/components/App.js
@@ -2,25 +2,27 @@ import React from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 
+
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        axios.get('http://api.unsplash.com/search/photos', {
-            params: { query: term },
-            headers: {
-                Authorization: 'Client-ID 8fe60030b2a01ad2cfe8ea65b2956c1b9d41f24182e5e19af278ac6004f75b64'
-            }
-        }).then((response) => {
-            console.log(response);
-        })
-    }
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+                params: { query: term },
+                headers: {
+                    'Access-Control-Allow-Credentials':true,
+                    Authorization: 'Client-ID 8fe60030b2a01ad2cfe8ea65b2956c1b9d41f24182e5e19af278ac6004f75b64'
+                }
+            });
+
+            console.log(response.data.results);
+        }
 
     render() {
         return (
             <div className ="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
             </div>
-        )
+        );
     }
 }
 
