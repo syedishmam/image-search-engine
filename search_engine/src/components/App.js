dc8963716f7d6b8c90eb1cecb78b4ab4d36fd924
Ish <syedishmam17@gmail.com>
2019-08-15 19:17:33 -0400

    Fix "this" binding issue

--- search_engine/src/components/App.js
+++ search_engine/src/components/App.js
@@ -5,7 +5,9 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-    async onSearchSubmit(term) {
+    state = { images: [] }
+
+    onSearchSubmit = async (term) => {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
                 params: { query: term },
                 headers: {
@@ -13,14 +15,15 @@ class App extends React.Component {
                     Authorization: 'Client-ID 8fe60030b2a01ad2cfe8ea65b2956c1b9d41f24182e5e19af278ac6004f75b64'
                 }
             });
-
-            console.log(response.data.results);
+            this.setState({ images: response.data.results });
         }
+        
 
     render() {
         return (
             <div className ="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
