c7cc6a69283ac3e08bfa4720464446e6ee120a00
Ish <syedishmam17@gmail.com>
2019-08-15 21:39:12 -0400

    Adjust span of images based on image height

--- search_engine/src/components/ImageCard.js
+++ search_engine/src/components/ImageCard.js
@@ -3,24 +3,29 @@ import React from 'react';
 class ImageCard extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { spans: 0 };
 
         this.imageRef = React.createRef();
     }
 
     //When each image loads call setSpans()
     componentDidMount() {
-        this.imageRed.current.addEventListener('load', this.setSpans)
+        this.imageRef.current.addEventListener('load', this.setSpans)
     }
 
+    //Sets number of spans needed for each image based on it's height
     setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 150 + 1);
 
+        this.setState({ spans });
     }
 
     render() {
         const {description, urls} = this.props.image;
 
         return (
-            <div>
+            <div style={{gridRowEnd: `span ${this.state.spans}`}} >
                 <img ref={this.imageRef} alt={description} src={urls.regular} />
             </div>
         )
